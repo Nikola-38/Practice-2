43b018f297e2a55a75df89302d6bc39ed2db6895
Nikola-38 <163429541+Nikola-38@users.noreply.github.com>
2024-05-22 17:58:17 +0700

    Update #1.cpp

--- #1.cpp
+++ #1.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 using namespace std;
 
+//Проверка числа на простоту
 bool isPrime(int n) {
     if (n <= 1) return false;
     if (n <= 3) return true;
@@ -10,6 +11,20 @@ bool isPrime(int n) {
         if (n % i == 0 || n % (i + 2) == 0) return false;
     }
     return true;
+}
+
+//Возведение в степень по модулю
+int pow_mod(int a, int x, int p) {
+    a %= p; // Уменьшите значение по модулю p, чтобы оно находилось в пределах диапазона [0, p)
+    int result = 1;
+    while (x > 0) {
+        if (x % 2 == 1) {
+            result = (result * a) % p; // Умножьте результат на a и уменьшите по модулю p
+        }
+        a = (a * a) % p; // Возводим в квадрат a и уменьшаем по модулю p
+        x /= 2; // Разделите показатель степени на 2
+    }
+    return result;
 }
 
 // Функция для вычисления наибольшего общего делителя
@@ -42,45 +57,37 @@ int func_Euler(int n) {
     return result;
 }
 
-int pow_mod(int a, int x, int p) {
-    a %= p; // Reduce a modulo p to ensure it's within the range [0, p)
-    int result = 1;
-    while (x > 0) {
-        if (x % 2 == 1) {
-            result = (result * a) % p; // Multiply the result by a and reduce modulo p
-        }
-        a = (a * a) % p; // Square a and reduce modulo p
-        x /= 2; // Divide the exponent by 2
-    }
-    return result;
-}
 
 int main() {
-    setlocale(LC_ALL, "Rus");
-    int a = 0, x = 0, p = 0;
-    cout << "Введите основание числа: ";
+  system("chcp 65001");
+    int a, x, p, b, y;
+    cout << "Введите основание первого числа: ";
     cin >> a;
-    cout << "Введите степень числа: ";
+    cout << "Введите степень первого числа числа: ";
     cin >> x;
+    cout << "Введите основание второго числа: ";
+    cin >> b;
+    cout << "Введите степень второго числа числа: ";
+    cin >> y;
     cout << "Введите модуль числа: ";
     cin >> p;
 
 
-    if (isPrime(p)) {
+    if (isPrime(p) && a % p != 0 ) {
         int result = pow_mod(a, p-1, p);
-        if (result == 1) {
-            cout << "Согласно теореме Ферма, " << a << "^" << (p - 1) << " mod " << p << " = 1" << endl;
+        if (result == 1 ) {
+            cout << "Согласно теореме Ферма, " << a << "^" << x << " mod " << p << " = 1" << endl;
            
         }
         else {
-            cout << "Теорема Ферма не выполняется для " << a << "^" << (p - 1) << " mod " << p << endl;
+            cout << "Теорема Ферма не выполняется для " << a << "^" << x << " mod " << p << endl;
         }
     }
     else {
         cout << "Условие теоремы Ферма не выполняется для введенных значений." << endl;
     }
 
-    if (p % 2 == 0 && a % p != 0) {
+    if (p % 2 == 0 || p % 2 !=0) {
         int eulerResult = pow_mod(a, func_Euler(x), p);
         if (eulerResult == 1) {
             cout << "Согласно теореме Эйлера, " << a << "^" << x << " mod " << p << " = 1" << endl;
@@ -95,10 +102,16 @@ int main() {
     }
 
     int resultProperties = pow_mod(a, x, p);
-    int resultLogarithm = pow_mod(a, x, p); // Assuming we have a logarithm function
-
-    cout << "Результат, используя свойства сравнений = " << resultProperties << endl;
-    cout << "Результат, используя логарифм = " << resultLogarithm << endl;
+    int resultProperties1 = pow_mod(b, y, p);
+    if(resultProperties == resultProperties1){
+         cout << "Результат, используя свойства сравнений: " << a << "^" << x << " mod " << p <<" = "<< b << "^" << y << " mod " << p <<" = " <<resultProperties << endl;
+    } else {
+         cout << "Результат, используя свойства сравнений: "  << a << "^" << x << " mod " << p <<" != "<< b << "^" << y << " mod " << p << endl;
+    }
+    int resultLogarithm = pow_mod(a, x, p); 
+    int resultLogarithm1 = pow_mod(b, y, p); 
+    cout << "Результат, используя логарифм для первого числа = " << resultLogarithm << endl;
+    cout << "Результат, используя логарифм для второго числа = " << resultLogarithm1 << endl;
 
     return 0;
 }
